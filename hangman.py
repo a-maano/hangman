ce934e847a1b6bea811ef906e06d0900081edbc7
Aaron Maaño <acmaano1@up.edu.ph>
2024-03-28 21:38:40 +0800

    added alpha and length guess checker

--- hangman.py
+++ hangman.py
@@ -193,27 +193,33 @@ def play(word, secret):
 			print(hanger[8])
 			print("You loose!")
 			break
+
 		else:
 			print(hanger[chance])
 			updatePrompt(secret)
 			print("Guess a letter: ",end="")
 			guess = input().upper()
+			print("\n======================")
 
-			if guess in letters_guessed:
-				print("You already have guessed that letter!")
+			if len(guess) > 1 or not guess.isalpha():
+				print("Invalid input!")
 			else:
-				letters_guessed.append(guess)
-				if guess in word:
-					indices = [i for i, x in enumerate(word) if x == guess]
-					for i in indices:
-						secret[i] = guess
+				if guess in letters_guessed:
+					print("You already have guessed that letter!")
 				else:
-					chance = chance + 1
+					letters_guessed.append(guess)
+					if guess in word:
+						indices = [i for i, x in enumerate(word) if x == guess]
+						for i in indices:
+							secret[i] = guess
+					else:
+						chance = chance + 1
 
-			if "_" not in secret:
-				print(hanger[9])
-				print("You win!")
-				break
+				if "_" not in secret:
+					print(hanger[9])
+					print("You win!")
+					break
+			print("Letters guessed:")
 			print(letters_guessed)
 			
 
@@ -222,7 +228,7 @@ while True:
 
 	choice = menu()
 	if choice == "1":
-		word = "aaron"
+		word = "rizz"
 		secret = ["_"]*len(word)
 		play(word.upper(),secret)
 	elif choice == "2":
@@ -231,18 +237,3 @@ while True:
 	else:
 		print("Invalid input!")
 	# os.system('cls' if os.name == 'nt' else 'clear')
-
-
-			
-# ╭┬───┐
-# ││         │
-# ││    ╭─┴─╮
-# ││    │x_x  │
-# ││    ╰─┬─╯
-# ││       ╭┼╮
-# ││       │││
-# ││      ╭┴╮
-# ││      │  │
-# ││     
-# ││
-# ╧╧═══════════
